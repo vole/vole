7dfc34f4d0cf2dc7516dea31601c878336d72059
Aaron Forsander <aaron.forsander@bazaarvoice.com>
2013-05-16 04:38:02 -0500

    Starting to abstract the database.

---
+++ src/db.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	// "github.com/petar/GoLLRB/llrb"
+	"fmt"
+	"github.com/nu7hatch/gouuid"
+	"io/ioutil"
+	"path"
+	"runtime"
+	"strings"
+	"time"
+)
+
+const VERSION = "v1"
+
+var DIR = func() string {
+	_, filename, _, _ := runtime.Caller(1)
+	return path.Join(path.Dir(filename), "data")
+}()
+
+func ReadFile(args ...string) ([]byte, error) {
+	return ioutil.ReadFile(path.Join(args...))
+}
+
+func ReadDir(args ...string) ([]os.FileInfo, error) {
+	return ioutil.ReadDir(path.Join(args...))
+}
+
+func Create(args ...string) (*os.File, error) {
+	return os.Create(path.Join(args...))
+}
+
+/**
+ * Posts
+ */
+
+type Post struct {
+	Uuid    string `json:"uuid"`
+	Title   string `json:"title"`
+	User    string `json:"user"`
+	Created int64  `json:"created"`
+}
+
+type PostContainer struct {
+	Post Post `json:"post"`
+}
+
+type PostCollection struct {
+	Posts []Post `json:"posts"`
+}
+
+func (post *Post) Save() error {
+	if post.Uuid == "" {
+		uuid, _ := uuid.NewV4()
+		post.Uuid = fmt.Sprintf("%s", uuid)
+	}
+
+	if post.Created == 0 {
+		post.Created = time.Now().UnixNano()
+	}
+
+	rawJson, err := json.Marshal(*post)
+	if err != nil {
+		return err
+	}
+
+	file, err := Create(DIR, "users", post.User, VERSION, "posts", post.FileName())
+	if err != nil {
+		return err
+	}
+
+	file.Write(rawJson)
+	return nil
+}
+
+func (post *Post) FileName() string {
+	return fmt.Sprintf("%d-post-%s", post.Created, post.Uuid)
+}
+
+func PostFromJson(rawJson []byte) *Post {
+	var post Post
+	json.Unmarshal(rawJson, &post)
+	return &post
+}
+
+func PostContainerFromJson(rawJson []byte) *PostContainer {
+	var container PostContainer
+	json.Unmarshal(rawJson, &container)
+	return &container
+}
+
+func NewPost(title string, user string) *Post {
+	return &Post{
+		Title: title,
+		User:  user,
+	}
+}
+
+func NewPostContainer(post *Post) *PostContainer {
+	return &PostContainer{Post: *post}
+}
+
+func NewPostCollection(posts []Post) *PostCollection {
+	return &PostCollection{Posts: posts}
+}
+
+func getPosts() (*PostCollection, error) {
+	posts := make([]Post, 0)
+
+	users, _ := ReadDir(DIR, "users")
+
+	for _, user := range users {
+		dir := path.Join(DIR, "users", user.Name(), VERSION, "posts")
+
+		files, err := ReadDir(dir)
+		if err != nil {
+			continue
+		}
+
+		for _, post := range files {
+			data, err := ReadFile(dir, post.Name())
+
+			if err != nil {
+				continue
+			}
+
+			posts = append(posts, *PostFromJson(data))
+		}
+	}
+
+	return &PostCollection{posts}, nil
+}
+
+/**
+ * Users
+ */
+
+type User struct {
+	Key         string `json:"key"`
+	Hash        string `json:"hash"`
+	User        string `json:"user"`
+	DisplayName string `json:"display_name"`
+}
+
+type UserCollection struct {
+	Users []User `json:"users"`
+}
+
+type UserContainer struct {
+	User User `json:"user"`
+}
+
+func UserFromJson(rawJson []byte) *User {
+	var user User
+	json.Unmarshal(rawJson, &user)
+	return &user
+}
+
+func NewUser(user string, displayName string) *User {
+	return &User{
+		User:        user,
+		DisplayName: displayName,
+	}
+}
+
+func NewUserContainer(user *User) *UserContainer {
+	return &UserContainer{User: *user}
+}
+
+func NewUserCollection(users []User) *UserCollection {
+	return &UserCollection{Users: users}
+}
+
+func CurrentUser() (*User, error) {
+	rawName, err := ReadFile(DIR, "my_user")
+	if err != nil {
+		return nil, err
+	}
+
+	name := strings.TrimSpace(string(rawName))
+
+	data, err := ReadFile(DIR, "users", name, VERSION, "user", name)
+	if err != nil {
+		return nil, err
+	}
+
+	return UserFromJson(data), nil
+}
--- src/vole.go
+++ src/vole.go
@@ -1,134 +1,71 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "flag"
-  "strings"
-  "io/ioutil"
-  "time"
-  "github.com/vole/web"
-  "github.com/nu7hatch/gouuid"
-  "encoding/json"
+	"encoding/json"
+	"flag"
+	"github.com/vole/web"
+	"io/ioutil"
 )
 
-/**
- *
- * import "vole/db"
- *
- * db.findPosts(group)
- * db.findUsers(group)
- *
- * db.findUser(group, KEY)
- * db.findPost(group, GUID)
- *
- * db.savePost(group, post)
- *
- */
-
-func getAllPosts(ctx *web.Context) string {
-  ctx.SetHeader("Content-Type", "application/json", true)
-
-  posts := make([]string, 0)
-
-  users_info, _ := ioutil.ReadDir("./data/users")
-
-  for _, user_info := range users_info {
-    fmt.Println(string(user_info.Name()))
-    posts_dir := "./data/users/" + string(user_info.Name()) + "/v1/posts"
-    posts_info, _ := ioutil.ReadDir(posts_dir)
-
-    for _, post_info := range posts_info {
-      post_path := posts_dir + "/" + string(post_info.Name())
-      fmt.Println(post_path)
-
-      post_data, err := ioutil.ReadFile(post_path)
-      if err == nil {
-        posts = append(posts, string(post_data))
-      }
-    }
-  }
-
-  out := `{ "posts": [`
-  out += strings.Join(posts, ",")
-  out += `] }`
-
-  return out
-}
-
-func savePost(ctx *web.Context) string {
-  data, err := ioutil.ReadFile("data/my_user")
-  if err != nil {
-    ctx.Abort(500, "Couldn't determine current user.")
-  }
-
-  body, err := ioutil.ReadAll(ctx.Request.Body);
-
-  var post Post
-  json.Unmarshal(body, &post)
-
-  user := strings.TrimSpace(string(data))
-  ts := time.Now().UnixNano()
-  uuid, _ := uuid.NewV4()
-
-  filename := fmt.Sprintf("%d-post-%s", ts, uuid)
-  file, err := os.Create("data/users/" + user + "/v1/posts/" + filename)
-  if err != nil {
-    ctx.Abort(500, "Unable to create file.")
-  }
-
-  post.Post.Uuid = fmt.Sprintf("%s", uuid)
-
-  postBodyJson, _ := json.Marshal(post.Post)
-  postJson, _ := json.Marshal(post)
-
-  file.Write(postBodyJson)
-  return string(postJson)
-}
-
-func getMyUser(ctx *web.Context) string {
-  ctx.SetHeader("Content-Type", "application/json", true)
-
-  name := "";
-  data, err := ioutil.ReadFile("data/my_user")
-  if err == nil {
-    name = strings.TrimSpace(string(data))
-    fmt.Println("found my user: " + name)
-
-    // Load the user's profile json.
-    profile := ""
-    profile_data, profile_err := ioutil.ReadFile("data/users/" + name + "/v1/user/" + name)
-
-    if profile_err == nil {
-      profile = strings.TrimSpace(string(profile_data))
-    } else {
-      fmt.Println(profile_err)
-    }
-
-    return `{ "users": [` + profile + `] }`
-  }
-  fmt.Println(err)
-  return ""
-}
-
-type Post struct {
-  Post PostBody `json:"post"`
-}
-
-type PostBody struct {
-  Uuid string `json:"uuid"`
-  Title string `json:"title"`
-  User string `json:"user"`
-}
+var port = flag.String("port", "6789", "Port on which to run the web server.")
 
 func main() {
-  var port = flag.String("port", "6789", "Port on which to run the web server.")
-  flag.Parse()
+	flag.Parse()
 
-  web.Get("/api/posts", getAllPosts)
-  web.Get("/api/users", getMyUser)
+	web.Get("/api/posts", func(ctx *web.Context) string {
+		ctx.ContentType("json")
 
-  web.Post("/api/posts", savePost)
+		posts, err := getPosts()
+		if err != nil {
+			ctx.Abort(500, "Error loading posts.")
+		}
 
-  web.Run("0.0.0.0:" + *port)
+		postsJson, err := json.Marshal(*posts)
+		if err != nil {
+			ctx.Abort(500, "Error marshalling posts.")
+		}
+
+		return string(postsJson)
+	})
+
+	web.Get("/api/users", func(ctx *web.Context) string {
+		ctx.ContentType("json")
+
+		user, err := CurrentUser()
+		if err != nil {
+			ctx.Abort(500, "Error loading user.")
+		}
+
+		collection := NewUserCollection([]User{*user})
+
+		userJson, err := json.Marshal(collection)
+		if err != nil {
+			ctx.Abort(500, "Error marshalling user.")
+		}
+
+		return string(userJson)
+	})
+
+	web.Post("/api/posts", func(ctx *web.Context) string {
+		body, err := ioutil.ReadAll(ctx.Request.Body)
+		if err != nil {
+			ctx.Abort(500, "Error reading request body.")
+		}
+
+		container := PostContainerFromJson(body)
+
+		err = (*container).Post.Save()
+		if err != nil {
+			ctx.Abort(500, "Error saving post.")
+		}
+
+		containerJson, err := json.Marshal(*container)
+		if err != nil {
+			ctx.Abort(500, "Error marshalling post.")
+		}
+
+		return string(containerJson)
+	})
+
+	web.Run("0.0.0.0:" + *port)
 }
