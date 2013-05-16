f8b1cdbc9e11f2446a1b5f67641c4f53b4c0067e
Aaron Forsander <aaron.forsander@bazaarvoice.com>
2013-05-16 15:08:17 -0500

    Fix formatting.

--- src/db.go
+++ src/db.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/nu7hatch/gouuid"
-	"github.com/petar/GoLLRB/llrb"
-	"io/ioutil"
-	"os"
-	"path"
-	"runtime"
-	"strings"
-	"time"
+  "encoding/json"
+  "fmt"
+  "github.com/nu7hatch/gouuid"
+  "github.com/petar/GoLLRB/llrb"
+  "io/ioutil"
+  "os"
+  "path"
+  "runtime"
+  "strings"
+  "time"
 )
 
 const VERSION = "v1"
 
 var DIR = func() string {
-	_, filename, _, _ := runtime.Caller(1)
-	return path.Join(path.Dir(filename), "data")
+  _, filename, _, _ := runtime.Caller(1)
+  return path.Join(path.Dir(filename), "data")
 }()
 
 func ReadFile(args ...string) ([]byte, error) {
-	return ioutil.ReadFile(path.Join(args...))
+  return ioutil.ReadFile(path.Join(args...))
 }
 
 func ReadDir(args ...string) ([]os.FileInfo, error) {
-	return ioutil.ReadDir(path.Join(args...))
+  return ioutil.ReadDir(path.Join(args...))
 }
 
 func Create(args ...string) (*os.File, error) {
-	return os.Create(path.Join(args...))
+  return os.Create(path.Join(args...))
 }
 
 /**
@@ -37,105 +37,105 @@ func Create(args ...string) (*os.File, error) {
  */
 
 type Post struct {
-	Uuid    string `json:"uuid"`
-	Title   string `json:"title"`
-	User    string `json:"user"`
-	Created int64  `json:"created"`
-	llrb.Item
+  Uuid    string `json:"uuid"`
+  Title   string `json:"title"`
+  User    string `json:"user"`
+  Created int64  `json:"created"`
+  llrb.Item
 }
 
 type PostContainer struct {
-	Post Post `json:"post"`
+  Post Post `json:"post"`
 }
 
 type PostCollection struct {
-	Posts []Post `json:"posts"`
+  Posts []Post `json:"posts"`
 }
 
 func (post *Post) Save() error {
-	if post.Uuid == "" {
-		uuid, _ := uuid.NewV4()
-		post.Uuid = fmt.Sprintf("%s", uuid)
-	}
+  if post.Uuid == "" {
+    uuid, _ := uuid.NewV4()
+    post.Uuid = fmt.Sprintf("%s", uuid)
+  }
 
-	if post.Created == 0 {
-		post.Created = time.Now().UnixNano()
-	}
+  if post.Created == 0 {
+    post.Created = time.Now().UnixNano()
+  }
 
-	rawJson, err := json.Marshal(*post)
-	if err != nil {
-		return err
-	}
+  rawJson, err := json.Marshal(*post)
+  if err != nil {
+    return err
+  }
 
-	file, err := Create(DIR, "users", post.User, VERSION, "posts", post.FileName())
-	if err != nil {
-		return err
-	}
+  file, err := Create(DIR, "users", post.User, VERSION, "posts", post.FileName())
+  if err != nil {
+    return err
+  }
 
-	file.Write(rawJson)
-	return nil
+  file.Write(rawJson)
+  return nil
 }
 
 func (post *Post) FileName() string {
-	return fmt.Sprintf("%d-post-%s", post.Created, post.Uuid)
+  return fmt.Sprintf("%d-post-%s", post.Created, post.Uuid)
 }
 
 func (post *Post) Less(than llrb.Item) bool {
-	return post.Created < than.(*Post).Created
+  return post.Created < than.(*Post).Created
 }
 
 func PostFromJson(rawJson []byte) *Post {
-	var post Post
-	json.Unmarshal(rawJson, &post)
-	return &post
+  var post Post
+  json.Unmarshal(rawJson, &post)
+  return &post
 }
 
 func PostContainerFromJson(rawJson []byte) *PostContainer {
-	var container PostContainer
-	json.Unmarshal(rawJson, &container)
-	return &container
+  var container PostContainer
+  json.Unmarshal(rawJson, &container)
+  return &container
 }
 
 func NewPost(title string, user string) *Post {
-	return &Post{
-		Title: title,
-		User:  user,
-	}
+  return &Post{
+    Title: title,
+    User:  user,
+  }
 }
 
 func NewPostContainer(post *Post) *PostContainer {
-	return &PostContainer{Post: *post}
+  return &PostContainer{Post: *post}
 }
 
 func NewPostCollection(posts []Post) *PostCollection {
-	return &PostCollection{Posts: posts}
+  return &PostCollection{Posts: posts}
 }
 
-func getPosts() (*PostCollection, error) {
-	posts := make([]Post, 0)
+func GetPosts() (*PostCollection, error) {
+  posts := make([]Post, 0)
 
-	users, _ := ReadDir(DIR, "users")
+  users, _ := ReadDir(DIR, "users")
 
-	for _, user := range users {
-		dir := path.Join(DIR, "users", user.Name(), VERSION, "posts")
+  for _, user := range users {
+    dir := path.Join(DIR, "users", user.Name(), VERSION, "posts")
 
-		files, err := ReadDir(dir)
-		if err != nil {
-			continue
-		}
+    files, err := ReadDir(dir)
+    if err != nil {
+      continue
+    }
 
-		for _, post := range files {
-			data, err := ReadFile(dir, post.Name())
+    for _, post := range files {
+      data, err := ReadFile(dir, post.Name())
 
-			if err != nil {
-				continue
-			}
+      if err != nil {
+        continue
+      }
 
-			posts = append(posts, *PostFromJson(data))
-		}
-	}
+      posts = append(posts, *PostFromJson(data))
+    }
+  }
 
-	return &PostCollection{posts}, nil
+  return &PostCollection{posts}, nil
 }
 
 /**
@@ -143,55 +143,55 @@ func getPosts() (*PostCollection, error) {
  */
 
 type User struct {
-	Key         string `json:"key"`
-	Hash        string `json:"hash"`
-	User        string `json:"user"`
-	DisplayName string `json:"display_name"`
+  Key         string `json:"key"`
+  Hash        string `json:"hash"`
+  User        string `json:"user"`
+  DisplayName string `json:"display_name"`
 }
 
 type UserCollection struct {
-	Users []User `json:"users"`
+  Users []User `json:"users"`
 }
 
 type UserContainer struct {
-	User User `json:"user"`
+  User User `json:"user"`
 }
 
 func UserFromJson(rawJson []byte) *User {
-	var user User
-	json.Unmarshal(rawJson, &user)
-	return &user
+  var user User
+  json.Unmarshal(rawJson, &user)
+  return &user
 }
 
 func NewUser(user string, displayName string) *User {
-	return &User{
-		User:        user,
-		DisplayName: displayName,
-	}
+  return &User{
+    User:        user,
+    DisplayName: displayName,
+  }
 }
 
 func NewUserContainer(user *User) *UserContainer {
-	return &UserContainer{User: *user}
+  return &UserContainer{User: *user}
 }
 
 func NewUserCollection(users []User) *UserCollection {
-	return &UserCollection{Users: users}
+  return &UserCollection{Users: users}
 }
 
 func CurrentUser() (*User, error) {
-	rawName, err := ReadFile(DIR, "my_user")
-	if err != nil {
-		return nil, err
-	}
+  rawName, err := ReadFile(DIR, "my_user")
+  if err != nil {
+    return nil, err
+  }
 
-	name := strings.TrimSpace(string(rawName))
+  name := strings.TrimSpace(string(rawName))
 
-	data, err := ReadFile(DIR, "users", name, VERSION, "user", name)
-	if err != nil {
-		return nil, err
-	}
+  data, err := ReadFile(DIR, "users", name, VERSION, "user", name)
+  if err != nil {
+    return nil, err
+  }
 
-	return UserFromJson(data), nil
+  return UserFromJson(data), nil
 }
 
 /**
@@ -199,28 +199,28 @@ func CurrentUser() (*User, error) {
  */
 
 func GlobalPostTree() *llrb.LLRB {
-	tree := llrb.New()
+  tree := llrb.New()
 
-	users, _ := ReadDir(DIR, "users")
+  users, _ := ReadDir(DIR, "users")
 
-	for _, user := range users {
-		dir := path.Join(DIR, "users", user.Name(), VERSION, "posts")
+  for _, user := range users {
+    dir := path.Join(DIR, "users", user.Name(), VERSION, "posts")
 
-		files, err := ReadDir(dir)
-		if err != nil {
-			continue
-		}
+    files, err := ReadDir(dir)
+    if err != nil {
+      continue
+    }
 
-		for _, post := range files {
-			data, err := ReadFile(dir, post.Name())
+    for _, post := range files {
+      data, err := ReadFile(dir, post.Name())
 
-			if err != nil {
-				continue
-			}
+      if err != nil {
+        continue
+      }
 
-			tree.InsertNoReplace(PostFromJson(data))
-		}
-	}
+      tree.InsertNoReplace(PostFromJson(data))
+    }
+  }
 
-	return tree
+  return tree
 }
--- src/vole.go
+++ src/vole.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"github.com/vole/web"
-	"io/ioutil"
+  "encoding/json"
+  "flag"
+  "github.com/vole/web"
+  "io/ioutil"
 )
 
 var port = flag.String("port", "6789", "Port on which to run the web server.")
 
 func main() {
-	flag.Parse()
+  flag.Parse()
 
-	// tree := GlobalPostTree()
+  // tree := GlobalPostTree()
 
-	// tree.AscendGreaterOrEqual(tree.Min(), func(item llrb.Item) bool {
-	// 	post, _ := item.(*Post)
-	// 	fmt.Printf("%+v\n", post)
-	// 	return true
-	// })
+  // tree.AscendGreaterOrEqual(tree.Min(), func(item llrb.Item) bool {
+  // 	post, _ := item.(*Post)
+  // 	fmt.Printf("%+v\n", post)
+  // 	return true
+  // })
 
-	web.Get("/api/posts", func(ctx *web.Context) string {
-		ctx.ContentType("json")
+  web.Get("/api/posts", func(ctx *web.Context) string {
+    ctx.ContentType("json")
 
-		posts, err := getPosts()
-		if err != nil {
-			ctx.Abort(500, "Error loading posts.")
-		}
+    posts, err := GetPosts()
+    if err != nil {
+      ctx.Abort(500, "Error loading posts.")
+    }
 
-		postsJson, err := json.Marshal(*posts)
-		if err != nil {
-			ctx.Abort(500, "Error marshalling posts.")
-		}
+    postsJson, err := json.Marshal(*posts)
+    if err != nil {
+      ctx.Abort(500, "Error marshalling posts.")
+    }
 
-		return string(postsJson)
-	})
+    return string(postsJson)
+  })
 
-	web.Get("/api/users", func(ctx *web.Context) string {
-		ctx.ContentType("json")
+  web.Get("/api/users", func(ctx *web.Context) string {
+    ctx.ContentType("json")
 
-		user, err := CurrentUser()
-		if err != nil {
-			ctx.Abort(500, "Error loading user.")
-		}
+    user, err := CurrentUser()
+    if err != nil {
+      ctx.Abort(500, "Error loading user.")
+    }
 
-		collection := NewUserCollection([]User{*user})
+    collection := NewUserCollection([]User{*user})
 
-		userJson, err := json.Marshal(collection)
-		if err != nil {
-			ctx.Abort(500, "Error marshalling user.")
-		}
+    userJson, err := json.Marshal(collection)
+    if err != nil {
+      ctx.Abort(500, "Error marshalling user.")
+    }
 
-		return string(userJson)
-	})
+    return string(userJson)
+  })
 
-	web.Post("/api/posts", func(ctx *web.Context) string {
-		body, err := ioutil.ReadAll(ctx.Request.Body)
-		if err != nil {
-			ctx.Abort(500, "Error reading request body.")
-		}
+  web.Post("/api/posts", func(ctx *web.Context) string {
+    body, err := ioutil.ReadAll(ctx.Request.Body)
+    if err != nil {
+      ctx.Abort(500, "Error reading request body.")
+    }
 
-		container := PostContainerFromJson(body)
+    container := PostContainerFromJson(body)
 
-		err = (*container).Post.Save()
-		if err != nil {
-			ctx.Abort(500, "Error saving post.")
-		}
+    err = (*container).Post.Save()
+    if err != nil {
+      ctx.Abort(500, "Error saving post.")
+    }
 
-		containerJson, err := json.Marshal(*container)
-		if err != nil {
-			ctx.Abort(500, "Error marshalling post.")
-		}
+    containerJson, err := json.Marshal(*container)
+    if err != nil {
+      ctx.Abort(500, "Error marshalling post.")
+    }
 
-		return string(containerJson)
-	})
+    return string(containerJson)
+  })
 
-	web.Run("0.0.0.0:" + *port)
+  web.Run("0.0.0.0:" + *port)
 }
