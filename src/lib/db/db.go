e3bce089e9c740480dffccf0d1b4c527122110fa
Aaron Forsander <aaron.forsander@bazaarvoice.com>
2013-05-19 16:32:23 -0500

    Fix server error when my_user contains garbage.

--- src/lib/db/db.go
+++ src/lib/db/db.go
@@ -239,7 +239,7 @@ func GetUsers() (*UserCollection, error) {
 
     user, err := UserFromJson(data)
 
-    user.IsMyUser = (user.User == currentUser.User)
+    user.IsMyUser = (currentUser != nil && user.User == currentUser.User)
 
     collection = append(collection, *user)
   }
