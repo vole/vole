3c03b660e76c263510820e4f53e3a63dd8b8a8a6
Mark Theunissen <mark.theunissen@gmail.com>
2013-05-31 10:31:13 -0400

    Copy and chmod

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,8 +12,25 @@ module.exports = function(grunt) {
           {
             src: ['bin/vole'],
             dest: 'dist/vole'
+          },
+          {
+            src: ['bin/vole.exe'],
+            dest: 'dist/vole.exe'
+          },
+          {
+            src: ['config.sample.json'],
+            dest: 'dist/config.sample.json'
           }
         ]
+      }
+    },
+
+    chmod: {
+      options: {
+        mode: '755'
+      },
+      vole: {
+        src: ['dist/vole']
       }
     },
 
@@ -46,7 +63,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-chmod');
   grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask('default', ['jshint', 'clean', 'exec:install_vole', 'copy']);
+  grunt.registerTask('default', ['jshint', 'clean', 'exec:install_vole', 'copy', 'chmod']);
 };
