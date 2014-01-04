94735568c82462987ccac55f1923defaec4c49e7
Drew Wells <drew.wells00@gmail.com>
2014-01-04 13:48:09 -0600

    Markdown previews of posts
    Preview hide/shows based on value of textarea
    The composition textarea resizes based off input

--- static/js/app/postbox.js
+++ static/js/app/postbox.js
@@ -1,27 +1,41 @@
 
 define([
 	'flight/component',
-	'app/api'
-], function (component, API) {
+	'app/api',
+	'lib/marked'
+], function (component, API, marked) {
 
 	function postbox () {
 
 		this.defaultAttrs({
 			textareaSelector : 'textarea',
-			buttonSelector : '#post-button'
+			previewSelector  : '#preview-text',
+			buttonSelector   : '#post-button'
 		});
 
 		this.post = function () {
 			var textarea = this.select('textareaSelector');
-
+			this.select('previewSelector').html('');
 			API.post(textarea.val()).done(function () {
 				textarea.val('');
 			}.bind(this));
 		};
 
 		this.keyup = function () {
-			var disabled = (this.select('textareaSelector').val().length === 0);
+			var textarea = this.select('textareaSelector');
+			var disabled = (textarea.val().length === 0);
+			var height = textarea.innerHeight();
+			var scrollHeight = textarea[0].scrollHeight;
+
+			//Respond to overflows in textarea content
+			if( scrollHeight > height ){
+				textarea.height(scrollHeight);
+			}
+
 			this.select('buttonSelector').prop('disabled', disabled);
+			this.select('previewSelector')
+				.html(marked(textarea.val()))
+				.toggleClass('invisible', disabled);
 		};
 
 		this.after('initialize', function () {
